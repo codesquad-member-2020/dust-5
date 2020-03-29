dcd7125dc25d241ba3da253d2b64b3d9514420fd
delma <sdy2856@gmail.com>
2020-03-29 21:31:39 +0900

    Add: 예보화면 기본 ui 추가

---
+++ DustApp/DustApp/Controllers/ForecastViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ForecastViewController.swift
+//  DustApp
+//
+//  Created by delma on 2020/03/29.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    @IBOutlet var PMImage: UIImageView!
+    @IBOutlet var announceTextView: UITextView!
+    @IBOutlet var areaGradeTextView: UITextView!
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var playSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
