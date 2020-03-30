bee1d9d525d18836fb09ad163d6cc7218619b277
moonelysian <dandyblack@naver.com>
2020-03-30 21:31:22 +0900

    feature: 대기 예보 기능 - open api를 사용해서 대기 예보 데이터 가져옴 - 필요한 정보만 들어가게 다듬을 예정

---
+++ src/main/java/com/example/dust/controller/ForecastController.java
@@ -0,0 +1,49 @@
+package com.example.dust.controller;
+
+import com.example.dust.bean.ApiResponse;
+import com.example.dust.message.SuccessMessages;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sun.net.www.protocol.http.HttpURLConnection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+
+@RestController
+public class ForecastController {
+
+  @GetMapping("/forecast")
+  public ResponseEntity<ApiResponse> forecast() throws IOException {
+    String path = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth";
+    String serviceKey = "serviceKey=mcPOMk6d6ZIiWSfWF0W2X%2B49iH6SeYJyMG61uC1PfEVTWQC7rAepSWCYXt%2F3Rlb5MM2YGP92o28i5qupEEC6WA%3D%3D";
+    String searchDate = "searchDate=" +LocalDate.now().toString();
+    String informCode = "InformCode=PM10";
+    String returnType = "_returnType=json";
+
+    StringBuilder result = new StringBuilder();
+    BufferedReader bufferedReader = null;
+
+    URL url = new URL(path + "?"
+        + serviceKey + "&"
+        + searchDate + "&"
+        + informCode + "&"
+        + returnType);
+
+    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+    urlConnection.setRequestMethod("GET");
+
+    bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8));
+    String line;
+    while ((line = bufferedReader.readLine()) != null) {
+      result.append(line);
+    }
+    return new ResponseEntity<>(new ApiResponse(SuccessMessages.SUCCESS, result.toString()), HttpStatus.OK);
+  }
+}
