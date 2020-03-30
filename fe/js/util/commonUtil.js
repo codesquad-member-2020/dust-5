975546ab72bbf0a587a32de9a26a98acf7fbf355
sgdg <seungdeng17@gmail.com>
2020-03-30 21:44:27 +0900

    update : export를 하단에서 객체로 묶어서 한 번에 하게 함

--- fe/js/util/commonUtil.js
+++ fe/js/util/commonUtil.js
@@ -1,15 +1,22 @@
-export function getElement(target) {
+function getElement(target) {
     return document.querySelector(target);
 }
 
-export function getElements(target) {
+function getElements(target) {
     return document.querySelectorAll(target);
 }
 
-export function addClass(target, className) {
+function addClass(target, className) {
     target.classList.add(className);
 }
 
-export function removeClass(target, className) {
+function removeClass(target, className) {
     target.classList.remove(className);
+}
+
+export {
+    getElement,
+    getElements,
+    addClass,
+    removeClass,
 }
