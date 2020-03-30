405983131277e9cc755d51586a87e46efec2bffc
Byung Hyun Choi <choi8608@gmail.com>
2020-03-31 02:50:05 +0900

    refactor : `미세먼지 앱` 페이지 구현
    - ForecastController.java
      - testdata 구현 로직을 좀 더 구체화
    - metadata
      - ApiKey, ApiUrl 설정
    - UrlBuilder.java
      - Get 형태의 URL Build 메소드 생성
    
    #18

---
+++ src/main/java/com/example/dust/metadata/ApiKey.java
@@ -0,0 +1,8 @@
+package com.example.dust.metadata;
+
+public class ApiKey {
+  public static String forecastKey =
+      "mcPOMk6d6ZIiWSfWF0W2X%2B49iH6SeYJyMG61uC1PfEVTWQC7rAepSWCYXt%2F3Rlb5MM2YGP92o28i5qupEEC6WA%3D%3D";
+  public static String stationSearchKey =
+      "LFpE9Q72hSFnljuSktMvFC2UrVtz2OSKdZf8PePqPcXB4Ali63V5Q0la6WdtrugkIwsWXeKHctFYjTJr8X%2FVGg%3D%3D";
+}
