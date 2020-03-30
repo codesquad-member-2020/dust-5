9d61d5e87bf0c7f89bd7857f1f961513a2f848d8
delma <sdy2856@gmail.com>
2020-03-30 22:16:43 +0900

    add: 24시간 측정값 받아올 모델 생성 #6

---
+++ DustApp/DustApp/Model/MeasuredHistory.swift
@@ -0,0 +1,14 @@
+//
+//  MeasuredHistory.swift
+//  DustApp
+//
+//  Created by delma on 2020/03/30.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+
+struct MeasuredHistory: Codable {
+    var pm10Value24: String
+    var dataTime: String
+}
