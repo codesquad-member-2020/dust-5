5a18d259437648d41268bbf494a53cef4a6d0994
sgdg <seungdeng17@gmail.com>
2020-03-30 21:32:30 +0900

    feat : dustAppEventManager module 추가함, 하단 타임라인 그래프 터치시 정보가 바뀌는 이벤트 구현
    - 사용자 터치 입력에 따라 하이라이트되는 그래프와 상단 정보가 변경되게 함

---
+++ fe/js/dustApp/dustAppEventManager.js
@@ -0,0 +1,50 @@
+const option = {
+    startPoint: 0,
+    endPoint: 0,
+    currSelectDataIndex: 0,
+    prevSelectDataIndex: 0,
+    prevDirection: 0
+}
+
+class DustAppEventManager {
+    constructor(module) {
+        this.dustAppModel = module.dustAppModel;
+        this.dustAppView = module.dustAppView;
+    }
+
+    addDustTimelineGraphTouchEvent() {
+        this.dustAppView.dustTimelineGraph.addEventListener('touchstart', this.touchstartHandle);
+        this.dustAppView.dustTimelineGraph.addEventListener('touchmove', this.touchmoveHandle.bind(this));
+        this.dustAppView.dustTimelineGraph.addEventListener('touchend', this.touchendHandle);
+    }
+    touchstartHandle(evt) {
+        option.startPoint = evt.touches[0].clientY;
+    }
+
+    touchmoveHandle(evt) {
+        option.endPoint = evt.touches[0].clientY;
+        const direction = parseInt((option.startPoint - option.endPoint) / 10);
+        option.prevSelectDataIndex = option.currSelectDataIndex;
+        if (direction > option.prevDirection) {
+            option.currSelectDataIndex += 1;
+            option.currSelectDataIndex >= this.dustAppModel.dustData.length - 1 ? option.currSelectDataIndex = this.dustAppModel.dustData.length - 1 : option.currSelectDataIndex;
+        } else if (direction < option.prevDirection) {
+            option.currSelectDataIndex -= 1;
+            option.currSelectDataIndex <= 0 ? option.currSelectDataIndex = 0 : option.currSelectDataIndex;
+        }
+        option.prevDirection = direction;
+        this.changeDisplayDustData();
+    }
+
+    changeDisplayDustData() {
+        this.dustAppModel.registerData(this.dustAppModel.dustData, option.currSelectDataIndex);
+        this.dustAppView.updateDustState(this.dustAppModel.displayDustData, this.dustAppModel.stationInfo);
+        this.dustAppView.highlightSelectDustData(option.currSelectDataIndex, option.prevSelectDataIndex);
+    }
+
+    touchendHandle() {
+        option.prevDirection = 0;
+    }
+}
+
+export default DustAppEventManager;
