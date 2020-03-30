f77f390e6aea5c4b711675bd1947762b745f8e84
Byung Hyun Choi <choi8608@gmail.com>
2020-03-30 21:54:15 +0900

    feat : pid 파일 생성
    - pid 파일 생성
    
    #15

--- src/main/java/com/example/dust/DustApplication.java
+++ src/main/java/com/example/dust/DustApplication.java
@@ -1,12 +1,15 @@
 package com.example.dust;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class DustApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(DustApplication.class, args);
+    SpringApplicationBuilder app = new SpringApplicationBuilder(DustApplication.class);
+    app.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
+    app.run();
   }
 }
