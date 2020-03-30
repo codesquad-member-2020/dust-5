02b1487f541c70e567e645591d6adb041e2dd8e4
moonelysian <dandyblack@naver.com>
2020-03-30 19:30:02 +0900

    init: be 기본 설정

---
+++ src/test/java/com/example/dust/DustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
