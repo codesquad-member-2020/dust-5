e931ce3fc51b708573d79033e72039c879a592be
Byung Hyun Choi <choi8608@gmail.com>
2020-03-30 20:01:44 +0900

    init : 기본 클래스 추가
    - WebConfig.java
      - CORS 설정
    - message
      - default message 설정
    - exception
      - default exception 설정
    - ApiResponse.java
      - default response 설정
    
    #15

--- src/main/java/com/example/dust/DustApplication.java
+++ src/main/java/com/example/dust/DustApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DustApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DustApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DustApplication.class, args);
+  }
 }
---
+++ src/main/java/com/example/dust/message/ErrorMessages.java
@@ -0,0 +1,7 @@
+package com.example.dust.message;
+
+public class ErrorMessages {
+  public static final String ERROR = "ERROR";
+  public final static String NOTFOUND_PATH = "해당 경로를 찾을 수 없습니다";
+  public final static String FORBIDDEN = "접근 권한이 없습니다";
+}
