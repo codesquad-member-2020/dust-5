96124132a712695a9df1ddeb2b312d379cd3599d
moonelysian <dandyblack@naver.com>
2020-03-30 21:41:38 +0900

    fix: controller 구조 변경 - 오타 수정 - controller 두개 하나로 합침

--- src/main/java/com/example/dust/controller/ForecastController.java
+++ src/main/java/com/example/dust/controller/ForecastController.java
@@ -3,9 +3,12 @@ package com.example.dust.controller;
 import com.example.dust.bean.ApiResponse;
 import com.example.dust.message.SuccessMessages;
 
+import com.example.dust.testdata.TestData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sun.net.www.protocol.http.HttpURLConnection;
 
@@ -16,10 +19,19 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 
+@Slf4j
 @RestController
+@RequestMapping("/forecast")
 public class ForecastController {
 
-  @GetMapping("/forecast")
+  @GetMapping("/dust-status")
+  public ResponseEntity<ApiResponse> dustStatus() {
+    log.debug("### dustStatus");
+
+    return new ResponseEntity<>(new ApiResponse(SuccessMessages.SUCCESS, TestData.dustStatus), HttpStatus.OK);
+  }
+
+  @GetMapping
   public ResponseEntity<ApiResponse> forecast() throws IOException {
     String path = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMinuDustFrcstDspth";
     String serviceKey = "serviceKey=mcPOMk6d6ZIiWSfWF0W2X%2B49iH6SeYJyMG61uC1PfEVTWQC7rAepSWCYXt%2F3Rlb5MM2YGP92o28i5qupEEC6WA%3D%3D";
