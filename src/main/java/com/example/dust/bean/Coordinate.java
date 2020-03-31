fe0462ba8468005f1e7b52d27a17c376b45ecc31
Byung Hyun Choi <choi8608@gmail.com>
2020-03-31 22:30:15 +0900

    refactor : transferCoordinationType 추가 구현
    - Coordinate.java, CoordinateData.java 추가
    - kakao api 의 결과에서 documents 값을 통해 기상청으로 보낼 tmX, tmY 추출

---
+++ src/main/java/com/example/dust/bean/Coordinate.java
@@ -0,0 +1,13 @@
+package com.example.dust.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class Coordinate {
+
+  private List<CoordinateData> documents;
+}
---
+++ src/main/java/com/example/dust/bean/CoordinateData.java
@@ -0,0 +1,11 @@
+package com.example.dust.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CoordinateData {
+  private String x;
+  private String y;
+}
