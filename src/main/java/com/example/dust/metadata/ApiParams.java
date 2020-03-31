50efa65504566ff57a07741e234ac1ca3b52b516
moonelysian <dandyblack@naver.com>
2020-03-31 15:10:57 +0900

    refactor: openApi 호출 함수utill로 빼냄 - urlbuilder 매우 좋다고 생각했지만 map으로 넣을 때 너무 하드코딩 느낌이라 뺌 - 지금처럼 붙여넣는게 나중에 파라미터 추가할때도 더 보기 쉬울꺼란 생각

--- src/main/java/com/example/dust/metadata/ApiParams.java
+++ src/main/java/com/example/dust/metadata/ApiParams.java
@@ -3,9 +3,15 @@ package com.example.dust.metadata;
 import java.time.LocalDate;
 
 public class ApiParams {
-  //대기예보
-  public static final String SERVICE_KEY = "ServiceKey=" + ApiKey.FORECAST_KEY;
+  public static final String FORECAST_SERVICE_KEY = "ServiceKey=" + ApiKey.FORECAST_KEY;
   public static final String RETURN_TYPE = "_returnType=json";
+
+  //대기예보
   public static final String SEARCH_DATE = "searchDate=" + LocalDate.now().toString();
   public static final String INFORM_CODE = "InformCode=PM10";
+
+  //미세먼지
+  public static final String STATION_NAME = "stationName=종로구";
+  public static final String DATA_TERM = "dataTerm=DAILY";
+  public static final String VERSION = "ver=1.3";
 }
