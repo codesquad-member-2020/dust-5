d6a2f50b38e2cfe2171bff260e117649868cb87f
moonelysian <dandyblack@naver.com>
2020-03-31 12:40:48 +0900

    fix: 오늘 날짜에 해당하는 예보만 반환하게 수정 - 기획서상 오늘 예보만 보는거 같아서 수정 - 아니면 다시 지울 수도 있음

--- src/main/java/com/example/dust/bean/Forecast.java
+++ src/main/java/com/example/dust/bean/Forecast.java
@@ -4,11 +4,23 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Forecast {
-  private List<Object> list;
+  @Getter
+  @Setter
+  private List<ForecastData> list;
+  private List<ForecastData> todayForecast = new ArrayList<>();
+
+  public List<ForecastData> getTodayForecast() {
+    for (ForecastData forecastData: list) {
+      if (forecastData.getInformData().equals(LocalDate.now().toString())) {
+        todayForecast.add(forecastData);
+      }
+    }
+    return todayForecast;
+  }
 }
