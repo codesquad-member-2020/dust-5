b97935eb658970a8ac95c53deef2e82732a67899
Byung Hyun Choi <choi8608@gmail.com>
2020-03-31 16:55:07 +0900

    feature : 미세먼지 측정을 위한 메소드 스켈레톤 코드 작성
    - ApiParams.java
      - 좌표변환, 측정소 정보 추가
    - ConnectionUtil.java
      - getResponseFromOpenAPi() 오버로딩
        - 기상청 API 는 get 방식에 키를 넣는 것에 비해 kakao api 는 header 에 들어갑니다
          따라서 로직이 달라지기에 오버로딩 하였으며 추후 리팩토링 예정입니다
    - ForecastController.java
      - dustStatus() -> getStation() -> transferCoord() 추가

--- src/main/java/com/example/dust/metadata/ApiParams.java
+++ src/main/java/com/example/dust/metadata/ApiParams.java
@@ -4,14 +4,27 @@ import java.time.LocalDate;
 
 public class ApiParams {
   public static final String FORECAST_SERVICE_KEY = "ServiceKey=" + ApiKey.FORECAST_KEY;
-  public static final String RETURN_TYPE = "_returnType=json";
+  public static final String STATION_SERVICE_KEY = "ServiceKey=" + ApiKey.STATION_SEARCH_KEY;
+
+  public static final String RETURN_TYPE_JSON = "_returnType=json";
 
   //대기예보
   public static final String SEARCH_DATE = "searchDate=" + LocalDate.now().toString();
   public static final String INFORM_CODE = "InformCode=PM10";
 
+  //좌표변환
+  public static final String X = "x=";
+  public static final String Y = "y=";
+  public static final String INPUT_COORD = "input_coord=WGS84";
+  public static final String OUTPUT_COORD = "output_coord=TM";
+
+  //측정소
+  public static final String TM_X = "tmX=";
+  public static final String TM_Y = "tmY=";
+  public static final String VER = "ver=1.0";
+
   //미세먼지
-  public static final String STATION_NAME = "stationName=종로구";
+  public static final String STATION_NAME = "stationName=";
   public static final String DATA_TERM = "dataTerm=DAILY";
   public static final String VERSION = "ver=1.3";
 }
