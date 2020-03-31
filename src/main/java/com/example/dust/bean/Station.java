46d2bb151dfebeae5976172f6931fa9afe9e59bb
Byung Hyun Choi <choi8608@gmail.com>
2020-03-31 22:38:06 +0900

    refactor : getStation() 추가 구현
    - Station.java, StationData.java 추가
    - TM 좌표를 기반으로 측정소 정보를 받아오고 첫번째 측정소를 기준으로 리턴합니다

---
+++ src/main/java/com/example/dust/bean/Station.java
@@ -0,0 +1,12 @@
+package com.example.dust.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Station {
+  private List<StationData> list;
+}
