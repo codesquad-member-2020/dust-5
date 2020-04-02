2c28814bf5b6c95f0a3c95cfc209f5a3496f95ae
moonelysian <dandyblack@naver.com>
2020-04-02 19:34:58 +0900

    fix: 오늘 전체 예보로 되돌리기 - 크롱이 사진 왜 3장밖에 없냐구 했다고 한다 - 췟

--- src/main/java/com/example/dust/bean/Forecast.java
+++ src/main/java/com/example/dust/bean/Forecast.java
@@ -1,6 +1,5 @@
 package com.example.dust.bean;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,19 +7,18 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Forecast {
   @Getter
   @Setter
   private List<ForecastData> list;
   private List<ForecastData> todayForecast = new ArrayList<>();
 
-  public ForecastData getTodayForecast() {
+  public List<ForecastData> getTodayForecast() {
     for (ForecastData forecastData: list) {
       if (forecastData.getInformData().equals(LocalDate.now().toString())) {
         todayForecast.add(forecastData);
       }
     }
-    return todayForecast.get(0);
+    return todayForecast;
   }
 }
