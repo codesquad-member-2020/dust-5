e6761908160913e60a3cb1afb2c20931db589f1a
delma <sdy2856@gmail.com>
2020-04-03 00:25:34 +0900

    feat: 서버 이미지 출력 및 재생, 일시정지 기능 구현

--- DustApp/DustApp/Controllers/ForecastViewController.swift
+++ DustApp/DustApp/Controllers/ForecastViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ForecastViewController: UIViewController {
-
+    
     @IBOutlet var PMImage: UIImageView!
     @IBOutlet var announceTextView: UITextView!
     @IBOutlet var areaGradeTextView: UITextView!
@@ -19,22 +19,60 @@ class ForecastViewController: UIViewController {
     let forecastManager = ForecastNetworkManager()
     private var isPlaying = false
     
+    let mock = Forecast(status: "SUCCESS", contents: ForecastMockDetail().mockDetail)
+    var forecastImages: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        forecastManager.requestForecastData { data in
-//        }
+//                forecastManager.requestForecastData { data in
+ //                   print(data)
+ //       }
+        setUpSlider()
+       stringToImage()
+    }
+    
+    func stringToImage()  {
+        let imageUrls = mock.contents.imageList
+        imageUrls.forEach { imageString in
+            
+            do {
+                guard let imageURL = URL(string: imageString) else { return }
+                let data = try Data(contentsOf: imageURL)
+                guard let image = UIImage(data: data) else { return }
+                forecastImages.append(image)
+            }catch {
+                
+            }
+        }
+        setPMImage(image: forecastImages[0])
+        PMImage.animationImages = forecastImages
+        PMImage.animationDuration = 2
+    }
+    
+    func setPMImage(image: UIImage) {
+        PMImage.image = image
+    }
+    
+    func setUpSlider() {
+        playSlider.minimumValue = 0
+        playSlider.maximumValue = 1
+        playSlider.value = 0.2
     }
     
     @IBAction func pressPlayButton(_ sender: UIButton) {
         if !isPlaying {
             playButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
             isPlaying = true
+            PMImage.startAnimating()
         }else {
             playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
             isPlaying = false
+            PMImage.stopAnimating()
         }
     }
     
-
-
+    @objc func sliderValueChange(_ sender: UISlider) {
+        
+    }
+    
 }
