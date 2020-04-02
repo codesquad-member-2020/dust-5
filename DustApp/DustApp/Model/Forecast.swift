1554b65ddbabc4c720154a11bb45d26e7da66c70
delma <sdy2856@gmail.com>
2020-04-02 19:51:06 +0900

    add: Forecast 모델 생성 #45

---
+++ DustApp/DustApp/Model/Forecast.swift
@@ -0,0 +1,23 @@
+//
+//  Forecast.swift
+//  DustApp
+//
+//  Created by delma on 2020/04/02.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+
+struct Forecast: Decodable {
+    var status: String
+    var contents: DetailInfo
+    
+    struct DetailInfo: Decodable {
+        var dataTime: String
+        var informCause: String
+        var informGrade: String
+        var informOverall: String
+        var informData: String
+        var imageList: [String]
+    }
+}
