42e224d874d0b05253b8f0fe5a1a7cca2c5fda0b
delma <sdy2856@gmail.com>
2020-04-03 12:45:30 +0900

    feat: 서버에서 받은 데이터 기반으로 예보화면에 보여주기

--- DustApp/DustApp/Controllers/ForecastViewController.swift
+++ DustApp/DustApp/Controllers/ForecastViewController.swift
@@ -19,29 +19,37 @@ class ForecastViewController: UIViewController {
     let forecastManager = ForecastNetworkManager()
     private var isPlaying = false
     
-    let mock = Forecast(status: "SUCCESS", contents: ForecastMockDetail().mockDetail)
     var forecastImages: [UIImage] = []
+    var forecast: Forecast?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//                forecastManager.requestForecastData { data in
- //                   print(data)
- //       }
-        setUpSlider()
-       stringToImage()
+        forecastManager.requestForecastData { data in
+            DispatchQueue.main.sync {
+                self.forecast = data
+                self.setUpSlider()
+                self.stringToImage()
+                self.setStringData()
+            }
+            
+        }
+        
     }
     
     func stringToImage()  {
-        let imageUrls = mock.contents.imageList
-        imageUrls.forEach { imageString in
-            
-            do {
-                guard let imageURL = URL(string: imageString) else { return }
-                let data = try Data(contentsOf: imageURL)
-                guard let image = UIImage(data: data) else { return }
-                forecastImages.append(image)
-            }catch {
+        guard let forecast = forecast else { return }
+        for i in 0..<forecast.contents.count {
+            let imageUrls = forecast.contents[i].imageList
+            imageUrls.forEach { imageString in
                 
+                do {
+                    guard let imageURL = URL(string: imageString) else { return }
+                    let data = try Data(contentsOf: imageURL)
+                    guard let image = UIImage(data: data) else { return }
+                    forecastImages.append(image)
+                }catch {
+                    
+                }
             }
         }
         setPMImage(image: forecastImages[0])
@@ -53,10 +61,16 @@ class ForecastViewController: UIViewController {
         PMImage.image = image
     }
     
+    func setStringData() {
+        guard let forecast = forecast else { return }
+        announceTextView.text = forecast.contents[0].informCause
+        areaGradeTextView.text = forecast.contents[0].informGrade
+    }
+    
     func setUpSlider() {
         playSlider.minimumValue = 0
         playSlider.maximumValue = 1
-        playSlider.value = 0.2
+        playSlider.value = 0.0
     }
     
     @IBAction func pressPlayButton(_ sender: UIButton) {
@@ -71,8 +85,11 @@ class ForecastViewController: UIViewController {
         }
     }
     
-    @objc func sliderValueChange(_ sender: UISlider) {
-        
-    }
+    
+    //전체 이미지 갯수로 slider의 value를 나눔
+    //각각의 이미지가 몫을 가짐
+    //슬라이더로 밸류값이 변경되면 그 밸류값에 해당하는 이미지가 표시
+    
+    
     
 }
