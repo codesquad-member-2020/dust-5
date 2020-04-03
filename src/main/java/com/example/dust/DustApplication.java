494af11dc070aed3475fee3fc05ec995b23f8e1b
moonelysian <dandyblack@naver.com>
2020-04-03 12:39:21 +0900

    fix: time zone 설정 - 우분투랑 스트링이랑 시간이 다름 - 설정해보기

--- src/main/java/com/example/dust/DustApplication.java
+++ src/main/java/com/example/dust/DustApplication.java
@@ -4,8 +4,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DustApplication {
+
+  @PostConstruct
+  public void started() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Seoul/Asia"));
+  }
 
   public static void main(String[] args) {
     SpringApplicationBuilder app = new SpringApplicationBuilder(DustApplication.class);
